d7b962b8fc7280919c715ac7ace969076e68b1bd
sudathmurari <sudathsudath@gmail.com>
2016-05-10 19:25:48 +0530

    cacheMatrix.R
    Solution to programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # "cache" will hold the cached matrix
+        # we shall set it to NULL initially
+        cache <- NULL
+        
+        # function definition to store a matrix
+        setMatrix <- function(newValue) {
+                x <<- newValue
+                # since the matrix is assigned a new value, flush the cache
+                cache <<- NULL
+        }
+        
+        # function definition to fetch the matrix
+        getMatrix <- function() {
+                x
+        }
+        
+        # function definition to cache the matrix
+        setInverse <- function(solve) {
+                cache <<- solve
+        }
+        
+        # function definition to fetch the cached matrix
+        getInverse <- function() {
+                cache
+        }
+        
+        # a list of functions is returned
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Function to calculate the inverse of a matrix
+# makeCacheMatrix
+cacheSolve <- function(y, ...) {
+        # get the inverse of y
+        inverse <- y$getInverse()
+        # if a cached value exists return it
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        # caclulate the inverse and store it in cache
+        data <- y$getMatrix()
+        inverse <- solve(data)
+        y$setInverse(inverse)
+        
+        # return inverse
+        inverse
 }
